5f84d795f8e94803392ec5b874a75a65740c4543
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-11-04 15:13:21 +0100

    Router

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './core/about/about.component';
+import { HomeComponent } from './core/home/home.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', // la partie de l'URL qui suit le slash et qui précède la query (?id=3)
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**', // wildcard
+    component: NotFoundComponent,
+    // redirectTo: '/',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { TodosModule } from './todos/todos.module';
 import { UsersModule } from './users/users.module';
+import { TopBarComponent } from './core/top-bar/top-bar.component';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
+    HomeComponent,
+    AboutComponent,
+    NotFoundComponent,
   ],
   imports: [ // equivalent au import ECMAScript pour les templates
     BrowserModule,
-    AppRoutingModule,
     SharedModule,
     TodosModule,
     UsersModule,
+    AppRoutingModule, // en dernier car contient la route **
   ],
   providers: [],
   bootstrap: [AppComponent]
