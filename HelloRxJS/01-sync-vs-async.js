90cdad741413f29abcb189af34ffae40936801d3
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-11-05 10:35:06 +0100

    Async

---
+++ HelloRxJS/01-sync-vs-async.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+// SYNC : tant que le fichier n'a pas été lu, on
+// ne passe pas à la ligne suivante
+// Le code est simple à lire
+// L'ordre des fichiers est garanti
+// Perf pas idéales
+// Le THREAD est bloqué sur le readFileSync
+// const bufferEC = fs.readFileSync('.editorconfig');
+// console.log(bufferEC.toString('utf-8'));
+
+// const bufferGI = fs.readFileSync('.gitignore');
+// console.log(bufferGI.toString('utf-8'));
+
+// ASYNC: le thread est libéré sur le readFile
+// le code est plus dur à lire du fait des callbacks
+// (pire avec la gestion d'erreur) -> callback hell
+// L'ordre des fichiers n'est pas garanti
+// Perf idéale, pendant la lecture d'un fichier on peut faire
+// autre chose (pas besoin de multithread pour ça)
+fs.readFile('.editorconfig', (err, bufferEC) => {
+  console.log(bufferEC.toString('utf-8'));
+});
+
+fs.readFile('.gitignore', (err, bufferGI) => {
+  console.log(bufferGI.toString('utf-8'));
+});
